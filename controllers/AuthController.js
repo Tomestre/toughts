530083f156d9a27f9467bc3b3b675cdef5322236
Tomestre <gabrielveleci@gmail.com>
2023-09-19 01:27:34 -0300

    auth login logic

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -7,6 +7,41 @@ module.exports = class AuthController {
         res.render('auth/login')
     }
 
+    static async loginPost(req,res){
+        const {email,password} = req.body
+
+        //find user
+        const user = await User.findOne({where: {email: email}})
+
+        // find User
+        if(!user){
+            req.flash('message','Usuário não encontrado!')
+            res.render('auth/login')
+
+            return
+        }
+
+        //check if passwords match
+        const passwordMatch = bcrypt.compareSync(password, user.password)
+
+        if(!passwordMatch){
+            req.flash('message','Senha Inválida')
+            res.render('auth/login')
+
+            return
+        }
+
+        //initialize session
+        req.session.userid = user.id
+
+        req.flash('message', 'Autenticação realizada com sucesso!')
+
+        req.session.save(()=> {
+            res.redirect('/')
+        })
+
+    }
+
     static register(req, res){
         res.render('auth/register')
     }
