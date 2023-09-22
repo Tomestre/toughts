4555eeb5a50e54822ca4c8b55dedf924b5b85023
Tomestre <gabrielveleci@gmail.com>
2023-09-22 00:04:00 -0300

    feat: insert toughts in to sql

--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -5,7 +5,10 @@ const ToughtController = require('../controllers/ToughtController')
 
 //helpers
 const checkAuth = require('../helpers/auth').checkAuth
+
+
 router.get('/add', checkAuth, ToughtController.createTought)
+router.post('/add', checkAuth, ToughtController.createToughtSave)
 
 router.get('/dashboard', checkAuth, ToughtController.dashboard)
 
