d5d8aa4ed11b42ca5ac7e9ada1c9373c93b0543a
Tomestre <gabrielveleci@gmail.com>
2023-10-05 01:28:15 -0300

    feat: edit part 1

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -1,3 +1,4 @@
+const { raw } = require('mysql2')
 const Tought = require('../models/Tought')
 const User = require('../models/User')
 
@@ -24,7 +25,15 @@ module.exports = class ToughtController{
 
         const toughts = user.Toughts.map((result)=> result.dataValues)
 
-        res.render('toughts/dashboard',{toughts})
+        let emptyToughts = false
+
+        if(toughts.length === 0){
+
+            emptyToughts = true
+
+        }
+
+        res.render('toughts/dashboard',{toughts, emptyToughts})
     }
 
     static createTought(req,res){
@@ -68,6 +77,16 @@ module.exports = class ToughtController{
     } catch(error){
             console.log('Aconteceu um erro:'+ error)
         }
+    }
+
+    static async updateTought(req, res){
+
+        const id = req.params.id
+
+        const tought = await Tought.findOne({where: {id:id} , raw: true} )
+
+        res.render('toughts/edit', {tought})
+
     }
 
 
